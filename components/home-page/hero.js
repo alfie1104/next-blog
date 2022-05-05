429cea16f8359dfbd5df0d5496cdbf0b931c0a5c
peter_alfie <alfie1104@gmail.com>
2022-05-06 08:02:54 +0900

    feat(hero.js): adding the "hero" component

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,2 +1,23 @@
-function Hero() {}
+import Image from "next/image";
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/alfie.png"
+          alt="An image showing alfie"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Alfie</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like React
+        or Next.
+      </p>
+    </section>
+  );
+}
 export default Hero;

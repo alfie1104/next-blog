4d078c890cec3c4bc0f30ea0809e8259c306c59a
peter_alfie <alfie1104@gmail.com>
2022-05-06 08:46:13 +0900

    feat(post-item, posts-grid): adding a posts grid and post items

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,2 +1,10 @@
-function FeaturedPosts() {}
+import classes from "./featured-posts.module.css";
+
+function FeaturedPosts() {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+}
 export default FeaturedPosts;

bf7628021faff1d78dd8428c3fbb57405aac0aea
peter_alfie <alfie1104@gmail.com>
2022-05-09 17:11:53 +0900

    feat(next.config.js): add config file and separate environtment variables to .env.*.local file

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const nextConfig = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        mongodb_username: process.env.mongodb_username,
+        mongodb_password: process.env.mongodb_password,
+        mongodb_clustername: process.env.mongodb_clustername,
+        mongodb_database: process.env.mongodb_database,
+      },
+    };
+  }
+
+  return {
+    reactStrictMode: true,
+    env: {
+      mongodb_username: process.env.mongodb_username,
+      mongodb_password: process.env.mongodb_password,
+      mongodb_clustername: process.env.mongodb_clustername,
+      mongodb_database: process.env.mongodb_database,
+    },
+  };
+};
+
+module.exports = nextConfig;
